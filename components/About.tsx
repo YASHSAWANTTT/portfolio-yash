bde888db99645aaf24636434302b0b4c234625be
Yash Sawant <137981875+YASHSAWANTTT@users.noreply.github.com>
2023-08-28 13:52:13 -0700

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -24,7 +24,7 @@ export default function About({}: Props) {
         }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true }}
-        src="https://scontent.xx.fbcdn.net/v/t1.15752-9/362146064_212924134669968_1549921968132321081_n.jpg?stp=dst-jpg_s403x403&_nc_cat=110&ccb=1-7&_nc_sid=aee45a&_nc_ohc=6t1NKY7MdJQAX_YC6BO&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdRBLdYlp86HQbBgGicqn2FpjZksoHesJGhbbE6P6NvBuA&oe=64E0A29D"
+        src="https://media.licdn.com/dms/image/D4D03AQHQFz1VCH7cLA/profile-displayphoto-shrink_400_400/0/1689805380591?e=1695254400&v=beta&t=sBNqbaAPjQtAuwMd5vrTF4iY18XuFmoiPv6kpUdE12I"
         className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[400px]   "
       />
 
